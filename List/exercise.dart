548c8cb5adc48eb1bdf0b22f1dc693fc52a120f6
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-02 01:02:30 +0600

    List Exercise done

---
+++ List/exercise.dart
@@ -0,0 +1,8 @@
+void main() {
+  //show two different ways of
+  //grabbing the last item in the list
+
+  var myList = ['A', 'B', 'C', 'D', 'E'];
+  print(myList[myList.length - 1]);
+  print(myList.last);
+}
