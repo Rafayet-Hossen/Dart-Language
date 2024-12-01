7c155896b2b20c12f211e39ef3c9f4ecf91b1c00
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-02 00:43:35 +0600

    String Exercise Done.

---
+++ String/Exercise.dart
@@ -0,0 +1,5 @@
+//replace all the x in the give string by using replaceAll method
+void main() {
+  var errorMessege = "Ohx Thex errorx occurx please keepx paxtience";
+  print(errorMessege.replaceAll('x', ''));
+}
