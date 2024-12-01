4542465b668702e0bde86e5e716115e2c6da5279
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-02 01:37:22 +0600

    Maps Exercise done

---
+++ Maps/Exercise.dart
@@ -0,0 +1,7 @@
+void main() {
+  var employee = {'Rafayet': 10, 'David': 5, 'Calvin': 3};
+  print(employee);
+  var removedValue = employee.remove('Rafayet');
+  print(removedValue);
+  print(employee);
+}
