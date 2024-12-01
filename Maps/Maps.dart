b0b829b49e90430fc0ba5fee6dcfca05b7068f97
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-02 01:33:10 +0600

    Maps Basics Done

---
+++ Maps/Maps.dart
@@ -0,0 +1,5 @@
+void main() {
+  var employee = {'Rafayet': 10, 'David': 5, 'Calvin': 3};
+  print(employee);
+  print(employee['Rafayet']);
+}
---
+++ Sets/Exercise1.dart
@@ -0,0 +1,9 @@
+void main() {
+  var set1 = {1, 3, 4, 5};
+  var set2 = {4, 2, 5, 7};
+  print(set1.intersection(set2));
+  print(set1.union(set2));
+}
+/**
+ * find the uinon and intersection of two set
+ */
