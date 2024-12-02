2d27c2e3925277dafcf26031b10c1e1ea5836680
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-02 16:07:43 +0600

    Loops basic ends

---
+++ Control Flow/ifelse/Loops/forloop.dart
@@ -0,0 +1,6 @@
+void main() {
+  var myList = ['a', 'b', 'c'];
+  for (var i = 0; i < myList.length; i++) {
+    print(myList[i]);
+  }
+}
---
+++ Control Flow/ifelse/Loops/iteration.dart
@@ -0,0 +1,22 @@
+void main() {
+  var myList = ['a', 'b', 'c', 'd'];
+  var mySet = {1, 4, 3, 6, 2};
+  var myMap = {'a': 1, 'b': 2, 'c': 3};
+
+  //for loop
+  for (final item in myList) {
+    print(item);
+  }
+  print('---------------------');
+  for (final key in myMap.keys) {
+    print(myMap[key]);
+  }
+  print('-----------------');
+  myMap.forEach((key, value) {
+    print('key: ${key} ==> value: ${value}');
+  });
+  print('------------------');
+  for (var i = 0; i < myList.length; i++) {
+    print(myList[i]);
+  }
+}
