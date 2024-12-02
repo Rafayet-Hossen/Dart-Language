98b5d65b43489a11cd7657b9da2d76317a0b3d5b
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-12-02 15:23:54 +0600

    if else statement

---
+++ Control Flow/ifelse/tut1.dart
@@ -0,0 +1,11 @@
+void main() {
+  var username = 'Gerg';
+  var storedName = 'Gerg';
+  print(username == storedName);
+
+  if (username == storedName) {
+    print("Welcome ${username}");
+  } else {
+    print("You are not register");
+  }
+}
